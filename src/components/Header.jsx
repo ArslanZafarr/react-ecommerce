f5ed7dc09563be1fba6697e7af577a1ac699dfca
Muhammad  Arslan <arslangreat1@gmail.com>
2023-07-03 10:27:12 +0500

    set routes

---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <div className='header-wrapper'>
+            <div className='header-container'>
+                <div className='header-brand'>
+                    <NavLink to="/">
+                        <h4>Bora Bora</h4>
+                    </NavLink>
+
+                </div>
+                <nav className='header-nav'>
+                    <li>
+                        <NavLink to="/" >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/about">
+                            About
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/products">
+                            Products
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact">
+                            Contact
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/cart">
+                            Cart
+                        </NavLink>
+                    </li>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Header
