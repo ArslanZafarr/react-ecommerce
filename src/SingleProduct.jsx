e8e729cb3125b3da44d84bba9452a4c4dd790b62
Muhammad  Arslan <arslanzafarr71@gmail.com>
2023-07-03 19:30:23 +0500

    design- single product page

--- src/SingleProduct.jsx
+++ src/SingleProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
 import { useProductContext } from './context/productcontext';
 
 const API = "https://api.pujakaitem.com/api/products/"
@@ -13,7 +13,8 @@ const SingleProduct = () => {
         getSingleProduct(`${API}${id}`)
     }, [])
 
-    const { name } = singleProduct
+    const { name, company, price, description, category, stock, reviews, stars, image } = singleProduct
+    console.log("🚀 ~ file: SingleProduct.jsx:17 ~ SingleProduct ~ image:", image)
 
     if (isSingleLoading) {
         return <>
@@ -26,7 +27,72 @@ const SingleProduct = () => {
 
     return (
         <>
-            <h1>Single Product {name} </h1></>
+            <section className='single-product-section'>
+                <div className='container-fluid single-product-container'>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <NavLink to='/'>
+                                <li class="breadcrumb-item">
+                                    Home
+                                </li>
+                            </NavLink>
+
+                            <li class="breadcrumb-item active" aria-current="page">{name}</li>
+                        </ol>
+                    </nav>
+                    <div className='single-product-content-wrapper'>
+                        <div className='single-product-features-image-gallery'>
+                            <div className='single-product-slider-images'>
+                                {image.map((curImage) => {
+                                    return <img src={curImage.url} alt="featured product image" />
+                                })}
+                            </div>
+                            <div className='singe-product-featured-image'>
+                                <img src={image[0].url} alt="featured product image" />
+                            </div>
+                        </div>
+                        <div className='single-product-details-content-wrapper'>
+                            <h3 className='product-title'>{name}</h3>
+                            <div className='product-rating-stars-and-reviews'>
+                                ({reviews} reviews)
+                            </div>
+                            <p>MRP <span>1999</span></p>
+                            <div className='deal-of-the-day-price'>
+                                <h5>Deal of the day:</h5>
+                                <h5>Rs {price}</h5>
+                            </div>
+                            <div className='single-product-description'>
+                                <p>{description}</p>
+                            </div>
+                            <div className='single-product-info'>
+                                <div>
+                                    <h5>Available :</h5>
+                                    <h6>In Stock</h6>
+                                </div>
+                                <div>
+                                    <h5>Brand :</h5>
+                                    <h6>{company}</h6>
+                                </div>
+                            </div>
+
+                            <div className='single-product-color-picker'>
+                                <h5>red </h5>
+                                <h5>red </h5>
+                                <h5>red </h5>
+                            </div>
+
+                            <div className='product-cart-item-increment-decrement'>
+                                <button>-</button>
+                                <p>1</p>
+                                <button>+</button>
+                            </div>
+
+                            <button className='add-to-cart-btn'>Add to Cart</button>
+                        </div>
+                    </div>
+                </div>
+            </section >
+        </>
     )
 }
 
