781759c1eecb9ab976b970ce7e4d0da59c0e153c
Muhammad  Arslan <arslanzafarr71@gmail.com>
2023-07-03 13:17:21 +0500

    CreateContext Api for Products

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import HeroSection from './components/HeroSection'
+import { useProductContext } from './context/productcontext'
+
 
 const Home = () => {
+  const myName = useProductContext();
   return (
     <>
+      <h1>  {myName}</h1>
       <HeroSection heading={"BORA BORA"} subHeading={"WELCOME TO"} imgUrl={"/images/hero.jpg"} />
     </>
   )
