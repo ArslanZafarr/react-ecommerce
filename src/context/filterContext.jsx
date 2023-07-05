468cbdbc92ed15f86f42421b412e2d98059e6dea
Muhammad  Arslan <arslanzafarr71@gmail.com>
2023-07-05 11:47:41 +0500

    Category Filter

--- src/context/filterContext.jsx
+++ src/context/filterContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
-import reducer from "../reducers/filterReducer"
+import reducer from "../reducers/filterReducer";
 import { useProductContext } from "./productcontext";
 
 const initialState = {
@@ -7,59 +7,70 @@ const initialState = {
     allProducts: [],
     gridView: true,
     sortingValue: "lowest",
-    filters : {
-        text : ""
-    }
-}
+    filters: {
+        text: "",
+        category : "all",
+        
+    },
+};
 
 const filterContext = createContext();
-
 
 const FilterContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
     const { products } = useProductContext();
 
     const isGridView = () => {
-        return dispatch({ type: "SET_GRID_VIEW" })
-    }
-
+        return dispatch({ type: "SET_GRID_VIEW" });
+    };
 
     const isListView = () => {
-        return dispatch({ type: "SET_LIST_VIEW" })
-    }
+        return dispatch({ type: "SET_LIST_VIEW" });
+    };
 
     useEffect(() => {
-        dispatch({ type: "SET_FILTER_PRODUCTS", payload: products })
-    }, [products])
+        dispatch({ type: "SET_FILTER_PRODUCTS", payload: products });
+    }, [products]);
 
-
-    // Search Filter 
+    // Search Filter
     const updateSearchFilter = (event) => {
-        let name = event.target.name
-        let value = event.target.value
-return  dispatch({type: "SEARCH_FILTER" ,  payload : {name , value}} )
-    }
+        let name = event.target.name;
+        let value = event.target.value;
+        return dispatch({ type: "SEARCH_FILTER", payload: { name, value } });
+    };
 
     // Price Sorting  Function
     const sorting = () => {
-        dispatch({ type: "SET_SORT_VALUE"  })
-    }
+        dispatch({ type: "SET_SORT_VALUE" });
+    };
 
-useEffect(() => { dispatch({type: "SEARCH_FILTER_RESULTS"});} , [state.filters])
+    useEffect(() => {
+        dispatch({ type: "SEARCH_FILTER_RESULTS" });
+    }, [state.filters]);
 
     // Price Sorting  Dispatch
-    useEffect(() => { 
-        dispatch({ type: "SORTING_PRODUCTS_PRICE", payload: products }) }, 
-        [products , state.sortingValue ])
+    useEffect(() => {
+        dispatch({ type: "SORTING_PRODUCTS_PRICE", payload: products });
+    }, [products, state.sortingValue]);
 
-    return <filterContext.Provider value={{ ...state, isGridView, isListView, sorting, updateSearchFilter }}>{children}</filterContext.Provider>
-}
+    return (
+        <filterContext.Provider
+            value={{
+                ...state,
+                isGridView,
+                isListView,
+                sorting,
+                updateSearchFilter,
+            }}
+        >
+            {children}
+        </filterContext.Provider>
+    );
+};
 
-// custom Hooks 
+// custom Hooks
 const useFilterContext = () => {
-    return useContext(filterContext)
-}
+    return useContext(filterContext);
+};
 
-
-
-export { FilterContextProvider, useFilterContext }
+export { FilterContextProvider, useFilterContext };
